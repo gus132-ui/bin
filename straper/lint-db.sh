e242bb24c739e16fb5d20842fcac73ee7e726409
Lukasz Kasprzak <lukasz.kasprzak@pm.me>
2026-03-19 22:00:55 +0100

    cleaned up the dir and some code

--- straper/lint-db.sh
+++ straper/lint-db.sh
@@ -3,7 +3,7 @@ set -Eeuo pipefail
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" && pwd)"
 # shellcheck disable=SC1091
-source "${SCRIPT_DIR}/common.sh"
+source "${SCRIPT_DIR}/lib/common.sh"
 
 SCRIPT_NAME="lint-db.sh"
 ISSUES=0
